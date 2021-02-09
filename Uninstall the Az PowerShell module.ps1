f2ed416fd8f729c5d78e6349f2ea212ccb7c0c5b
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-09 21:10:51 +0800

    Create Uninstall the Az PowerShell module.ps1

---
+++ Uninstall the Az PowerShell module.ps1
@@ -0,0 +1,108 @@
+# Uninstall the Az PowerShell module from PowerShellGet
+
+# To uninstall the Az modules, you can use the Uninstall-Module cmdlet. 
+# However, Uninstall-Module only uninstalls one module. 
+# To remove the Az PowerShell module completely, you must uninstall each module individually. 
+# Uninstallation can be complicated if you have more than one version of Azure PowerShell installed.
+
+Get-InstalledModule -Name Az -AllVersions # which version we have installed
+
+
+# The following script queries the PowerShell Gallery to get a list of dependent submodules. 
+# Then, the script uninstalls the correct version of each submodule. 
+# We need to have administrator access to run this script in a scope other than Process or Current User.
+
+function Uninstall-AzModule {
+  [CmdletBinding(SupportsShouldProcess)]
+  param(
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('Az','AzureRM','Azure')]
+    [string]$Name = 'Az',
+
+    [Parameter(Mandatory)]
+    [string]$Version,
+
+    [switch]$AllowPrerelease
+  )
+
+  $Params = @{}
+
+  if ($PSBoundParameters.AllowPrerelease) {
+    $Params.AllowPrerelease = $true
+  }
+
+  $IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
+
+  if (-not(Get-InstalledModule -Name $Name -RequiredVersion $Version -ErrorAction SilentlyContinue -OutVariable RootModule @Params)) {
+
+    Write-Warning -Message "Uninstall aborted. $Name version $Version not found."
+
+  } elseif (($RootModule.InstalledLocation -notlike "*$env:USERPROFILE*") -and ($IsAdmin -eq $false)) {
+
+    Write-Warning -Message "Uninstall aborted. $Name version $Version exists in a system path. PowerShell must be run elevated as an admin to remove it."
+
+  } elseif ((Get-Process -Name powershell, pwsh -OutVariable Sessions -ErrorAction SilentlyContinue).Count -gt 1) {
+
+    Write-Warning -Message "Uninstall aborted. Please close all other PowerShell sessions before continuing. There are currently $($Sessions.Count) PowerShell sessions running."
+
+  } else {
+    Write-Verbose -Message 'Creating list of dependencies...'
+    $target = Find-Module -Name $Name -RequiredVersion $Version @Params
+
+    $AllModules = @([pscustomobject]@{
+      Name = $Name
+      Version = $Version
+    })
+
+    $AllModules += foreach ($dependency in $target.Dependencies) {
+      switch ($dependency.keys) {
+        {$_ -contains 'RequiredVersion'} {$UninstallVersion = $dependency.RequiredVersion; break}
+        {$_ -contains 'MinimumVersion'} {$UninstallVersion = $dependency.MinimumVersion; break}
+      }
+
+      [pscustomobject]@{
+        Name = $dependency.Name
+        Version = $UninstallVersion
+      }
+    }
+
+    [int]$i = 100 / $AllModules.Count
+
+    foreach ($module in $AllModules) {
+      Write-Progress -Activity 'Uninstallation in Progress' -Status "$i% Complete:" -PercentComplete $i
+      $i++
+
+      if (Get-InstalledModule -Name $module.Name -RequiredVersion $module.Version -ErrorAction SilentlyContinue @Params) {
+        Write-Verbose -Message "Uninstalling $($module.Name) version $($module.Version)"
+
+        Remove-Module -FullyQualifiedName @{ModuleName=$module.Name;ModuleVersion=$module.Version} -ErrorAction SilentlyContinue
+
+        try {
+          if ($PSCmdlet.ShouldProcess("$($module.Name) version $($module.Version)")) {
+            Uninstall-Module -Name $module.Name -RequiredVersion $module.Version -Force -ErrorAction Stop @Params
+          }
+          $State = 'Uninstalled'
+        } Catch {
+          $State = 'Manual uninstall required'
+          Write-Verbose -Message "$($module.Name) version: $($module.Version) may require manual uninstallation."
+        }
+
+      } else {
+        $State = 'Not found'
+        Write-Verbose -Message "$($module.Name) version: $($module.Version) not found."
+      }
+
+      if (-not $PSBoundParameters.WhatIf) {
+        [pscustomobject]@{
+          ModuleName = $module.Name
+          Version = $module.Version
+          State = $State
+        }
+      }
+
+    }
+  }
+}
+
+
+Uninstall-AzModule -Name Az -Version 1.8.0
